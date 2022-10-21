8196189ec967b8651e721a813c6dbdf0ced6aed0
Ron Houser <115298564+RonHouser@users.noreply.github.com>
2022-10-20 21:33:49 -0400

    Update create_spotify_table.sql

--- Spotify/Create Tables/create_spotify_table.sql
+++ Spotify/Create Tables/create_spotify_table.sql
@@ -1,10 +1,3 @@
-/*The goal of this project was to demonstrate my ability to create tables and import data.
-In this example I sourced Spotify data from Kaggle.
-I created a table to insert the Spotify data into.
-I chose a few questions to demonstrate analytics on the data using SQL.
-I inserted the Spotify Data .csv into the table.
-I explored the data using the SQL below:*/ 
-
 CREATE TABLE BIT_DB.Spotifydata (
     id integer PRIMARY KEY,
     artist_name varchar NOT NULL,
@@ -24,28 +17,4 @@ CREATE TABLE BIT_DB.Spotifydata (
     tempo decimal(6,3) NOT NULL,
     duration_ms integer NOT NULL,
     time_signature integer NOT NULL);
-    
-/*What is the avg popularity, danceability, and energy by artist and track*/
-SELECT AVG(popularity), AVG(danceability),  AVG(energy), artist_name, track_name
-    FROM BIT_DB.Spotifydata
-    GROUP BY track_name;
-
-/*Who are the top 10 most popular artist?*/
-SELECT artist_name, popularity
-    FROM BIT_DB.Spotifydata
-    ORDER BY popularity desc
-    LIMIT 10;
-
-
-/*The next example uses a CTE to accomplish the task.
-Label every artist with an average popularity of 90 or above as a “Top Star”.*/  
-
-WITH pop_avg AS
   
-(    SELECT s.artist_name, AVG(s.popularity) AS AVG_Popularity
-        FROM SpotifyData AS s 
-        GROUP BY s.artist_name    )           
-                            
-SELECT  artist_name, AVG_Popularity, 'Top Star' AS tag
-    FROM pop_avg
-    WHERE AVG_Popularity >= 90;
